d8403869e011ab4c50e87fac7b886a3d70ce8a27
pang yew ken <ukenster@gmail.com>
2021-12-06 15:31:41 +0800

    seed info, index page

--- app/controllers/queue_walls_controller.rb
+++ app/controllers/queue_walls_controller.rb
@@ -2,7 +2,7 @@ class QueueWallsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @ranges = GolfRange.all
+    @queues = QueueWall.all
   end
 
   def new
