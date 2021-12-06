16d850281a590c2bbfc3f43674024556e9e41bc0
Joshua Eng <engch28@gmail.com>
2021-12-06 15:28:50 +0800

    change URL to access ENV key instead

--- app/controllers/queue_walls_controller.rb
+++ app/controllers/queue_walls_controller.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'open-uri'
 
 class QueueWallsController < ApplicationController
-  MAPBOX_API_KEY="pk.eyJ1IjoicGhwb2giLCJhIjoiY2t3bjd1Z3AyMjc2MDJ1bWxobXFzbGE4NCJ9.BpThnhSGxpTlguoJa3CuXg"
+
   def index
     travel_time
   end
@@ -16,7 +16,7 @@ class QueueWallsController < ApplicationController
   private
 
   def travel_time
-    url = "https://api.mapbox.com/directions/v5/mapbox/driving/103.8404503,1.3022018;103.8693475,1.2933014?access_token=#{MAPBOX_API_KEY}"
+    url = "https://api.mapbox.com/directions/v5/mapbox/driving/103.8404503,1.3022018;103.8693475,1.2933014?access_token=#{ENV['MAPBOX_API_KEY']}"
   # answer_serialized = URI.open(url).read
     movies = URI.parse(url).open.read
     movie_list = JSON.parse(movies)
