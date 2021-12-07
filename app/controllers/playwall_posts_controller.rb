7bc26c6257980ed724622385765532df343ee0f4
Joshua Eng <engch28@gmail.com>
2021-12-07 11:34:14 +0800

    created form to create new post and list all post

--- app/controllers/playwall_posts_controller.rb
+++ app/controllers/playwall_posts_controller.rb
@@ -1,10 +1,22 @@
 class PlaywallPostsController < ApplicationController
   def index
+    @playwall = PlaywallPost.all
+
   end
 
   def new
+    @playwall = PlaywallPost.new
   end
 
   def create
+    @playwall = PlaywallPost.new(post_params)
+    @playwall.user = current_user
+    redirect_to playwall_posts_path if @playwall.save
+  end
+
+  private
+
+  def post_params
+    params.require(:playwall_post).permit(:user_id, :caption, :golf_range_id)
   end
 end
