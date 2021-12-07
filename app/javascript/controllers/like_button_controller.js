eec342f5ac8d67ee0c84f76dcee58d64f4a1e022
Joshua Eng <engch28@gmail.com>
2021-12-07 16:00:21 +0800

    added like function to playwall post

---
+++ app/javascript/controllers/like_button_controller.js
@@ -0,0 +1,24 @@
+/**
+ * Stimulus controller
+ * Documentation: https://stimulus.hotwired.dev/
+ * read more about lifecyles at https://stimulus.hotwired.dev/reference/lifecycle-callbacks
+ */
+
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ "likebtn", "unlikebtn", "counter"];
+
+
+  like(event) {
+    this.likebtnTarget.className = "";
+    this.likebtnTarget.classList.add("far", "fa-heart");
+    this.counterTarget.innerText = parseInt(this.counterTarget.getAttribute("value")) - 1;
+  }
+
+  unlike(event) {
+    this.unlikebtnTarget.className = "";
+    this.unlikebtnTarget.classList.add("fas", "fa-heart", "text-danger");
+    this.counterTarget.innerText = parseInt(this.counterTarget.getAttribute("value")) + 1;
+  }
+}
