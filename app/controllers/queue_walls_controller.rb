39d77127354213fadb10a7d684f9d4154eb81048
Joshua Eng <engch28@gmail.com>
2021-12-07 16:28:08 +0800

    Added error handling for api errors

--- app/controllers/queue_walls_controller.rb
+++ app/controllers/queue_walls_controller.rb
@@ -25,10 +25,17 @@ class QueueWallsController < ApplicationController
   def travel_time(coordinates, ranges)
     time_array = []
     ranges.each do |place|
-      url = "https://api.mapbox.com/directions/v5/mapbox/driving/#{coordinates[1]},#{coordinates[0]};#{place.longitude},#{place.latitude}?access_token=#{ENV['MAPBOX_API_KEY']}"
-      mapbox_json = URI.parse(url).open.read
-      mapbox_info = JSON.parse(mapbox_json)
-      time_array << (mapbox_info["routes"].first["duration"] / 60).ceil
+      begin
+    #... process, may raise an exception
+        url = "https://api.mapbox.com/directions/v5/mapbox/driving/#{coordinates[1]},#{coordinates[0]};#{place.longitude},#{place.latitude}?access_token=#{ENV['MAPBOX_API_KEY']}"
+        mapbox_json = URI.parse(url).open.read
+        mapbox_info = JSON.parse(mapbox_json)
+      rescue SocketError
+          #... error handler
+        puts "API Error for mapbox, try again later"
+      else
+        time_array << (mapbox_info["routes"].first["duration"] / 60).ceil
+      end
     end
     time_array
   end
@@ -40,12 +47,18 @@ class QueueWallsController < ApplicationController
   def weather_status(ranges)
     weather_array = []
     ranges.each do |place|
-      url = "https://api.openweathermap.org/data/2.5/weather?lat=#{place.latitude}&lon=#{place.longitude}&appid=#{ENV['OPENWEATHER_API_KEY']}"
-      weather_json = URI.parse(url).open.read
-      weather_info = JSON.parse(weather_json)
-      weather_array << weather_info["weather"].first["icon"]
+      begin
+        #... process, may raise an exception
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=#{place.latitude}&lon=#{place.longitude}&appid=#{ENV['OPENWEATHER_API_KEY']}"
+        weather_json = URI.parse(url).open.read
+        weather_info = JSON.parse(weather_json)
+      rescue SocketError
+        #... error handler
+        puts "API Error for Open Weather, try again later"
+      else
+        weather_array << weather_info["weather"].first["icon"]
+      end
     end
     weather_array
   end
-
 end
