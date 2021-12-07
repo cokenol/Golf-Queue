4c73ca5779d6b811aafd9d23ae888cbb7c27d3c1
pang yew ken <ukenster@gmail.com>
2021-12-07 18:20:49 +0800

    show only latest post by time for each level

--- app/models/queue_wall.rb
+++ app/models/queue_wall.rb
@@ -4,6 +4,11 @@ class QueueWall < ApplicationRecord
   has_one_attached :photo
 
   scope :by_range, ->(given_id) { where(golf_range_id: given_id) }
-
+  scope :by_level_and_latest, -> () do
+    select("queue_walls.*")
+    .order("level DESC")
+    .group("level")
+    .maximum("created_at")
+  end
   acts_as_votable
 end
