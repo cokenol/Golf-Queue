9fdd790a50ba1f2a65652bc0f6f7a288c16a7797
Joshua Eng <engch28@gmail.com>
2021-12-13 15:06:39 +0800

    changed icons to reflect friend status

--- app/controllers/friendlists_controller.rb
+++ app/controllers/friendlists_controller.rb
@@ -1,5 +1,6 @@
 class FriendlistsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_friend, only: %i[add_friend block_friend unblock_friend delete_friend accept_friend decline_friend]
 
   def search_friend
     @users = User.all
@@ -13,36 +14,39 @@ class FriendlistsController < ApplicationController
   def friends_list
   end
 
-  # def show_friend
-  # end
-
   def add_friend
-    @friend = User.find(params[:id])
+    # @friend = User.find(params[:id])
     redirect_to friends_path if current_user.friend_request(@friend)
   end
 
   def block_friend
-    @friend = User.find(params[:id])
+    # @friend = User.find(params[:id])
     redirect_to friends_path if current_user.block_friend(@friend)
   end
 
   def unblock_friend
-    @friend = User.find(params[:id])
+    # @friend = User.find(params[:id])
     redirect_to friends_path if current_user.unblock_friend(@friend)
   end
 
   def delete_friend
-    @friend = User.find(params[:id])
+    # @friend = User.find(params[:id])
     redirect_to friends_path if current_user.remove_friend(@friend)
   end
 
   def accept_friend
-    @friend = User.find(params[:id])
+    # @friend = User.find(params[:id])
     redirect_to friends_path if current_user.accept_request(@friend)
   end
 
   def decline_friend
-    @friend = User.find(params[:id])
+    # @friend = User.find(params[:id])
     redirect_to friends_path if current_user.decline_request(@friend)
+  end
+
+  private
+
+  def find_friend
+    @friend = User.find(params[:id])
   end
 end
