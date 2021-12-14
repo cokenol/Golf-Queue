8257e4f56d1033c58895962e13c3fbdda81b89d6
Joshua Eng <engch28@gmail.com>
2021-12-14 17:54:06 +0800

    User status update will be reflected on status page and friendlist page

--- app/controllers/status_updates_controller.rb
+++ app/controllers/status_updates_controller.rb
@@ -6,8 +6,8 @@ class StatusUpdatesController < ApplicationController
   end
 
   def create
-    @start_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["starttime(4i)"].to_i,params["status_update"]["starttime(5i)"].to_i)
-    @end_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["endtime(4i)"].to_i,params["status_update"]["endtime(5i)"].to_i)
+    @start_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["starttime(4i)"].to_i,params["status_update"]["starttime(5i)"].to_i,00, '+8:00')
+    @end_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["endtime(4i)"].to_i,params["status_update"]["endtime(5i)"].to_i,00, '+8:00')
     @status = StatusUpdate.new(status_params)
     @golf_range = GolfRange.find(params["status_update"]["golf_range_id"])
     @status.golf_range = @golf_range
