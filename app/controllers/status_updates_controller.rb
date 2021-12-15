8af4a8115bc0704eee53c53dd88cf174924ad7c3
Joshua Eng <engch28@gmail.com>
2021-12-15 15:43:00 +0800

    prevent user from submitting empty status

--- app/controllers/status_updates_controller.rb
+++ app/controllers/status_updates_controller.rb
@@ -6,15 +6,20 @@ class StatusUpdatesController < ApplicationController
   end
 
   def create
-    @start_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["starttime(4i)"].to_i,params["status_update"]["starttime(5i)"].to_i,00, '+8:00')
-    @end_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["endtime(4i)"].to_i,params["status_update"]["endtime(5i)"].to_i,00, '+8:00')
-    @status = StatusUpdate.new(status_params)
-    @golf_range = GolfRange.find(params["status_update"]["golf_range_id"])
-    @status.golf_range = @golf_range
-    @status.start_time = @start_time
-    @status.end_time = @end_time
-    @status.user = current_user
-    redirect_to request.referer if @status.save
+    if params["status_update"]["start_date"].empty? || params["status_update"]["golf_range_id"].empty?
+      flash[:alert] = "Your start date and location cannot be empty!"
+      redirect_to request.referer
+    else
+      @start_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["starttime(4i)"].to_i,params["status_update"]["starttime(5i)"].to_i,00, '+8:00')
+      @end_time = DateTime.new(params["status_update"]["start_date"].split("-")[0].to_i,params["status_update"]["start_date"].split("-")[1].to_i,params["status_update"]["start_date"].split("-")[2].to_i,params["status_update"]["endtime(4i)"].to_i,params["status_update"]["endtime(5i)"].to_i,00, '+8:00')
+      @status = StatusUpdate.new(status_params)
+      @golf_range = GolfRange.find(params["status_update"]["golf_range_id"])
+      @status.golf_range = @golf_range
+      @status.start_time = @start_time
+      @status.end_time = @end_time
+      @status.user = current_user
+      redirect_to request.referer if @status.save
+    end
   end
 
   def new
